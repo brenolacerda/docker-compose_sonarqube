1267b9f1ab8fcaddc57d310f64d96dbac77d109d
Breno <brenolacerda@gmail.com>
2020-09-03 23:07:16 -0300

    Initial Commit

---
+++ deploySonarqube.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo yum update -y
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce docker-ce-cli containerd.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+docker-compose --version
+
+mkdir /opt/sonarqube/
+
+cd /opt/sonarqube/ 
+
+git clone https://github.com/brenolacerda/docker-compose_sonarqube.git
+
+/usr/local/bin/docker-compose up
